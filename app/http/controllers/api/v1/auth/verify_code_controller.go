f5cf0c192abd3011fee559a4db8dd69858bc0641
GGboya <630591905@qq.com>
2023-07-04 23:00:18 +0800

    发送邮件验证码

--- app/http/controllers/api/v1/auth/verify_code_controller.go
+++ app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/requests"
@@ -34,13 +35,32 @@ func (vc *VerifyCodeController) SendUsingPhone(c *gin.Context) {
 
 	// 1. 验证表单
 	request := requests.VerifyCodePhoneRequest{}
+	fmt.Println("走到了if ok 之前")
+
 	if ok := requests.Validate(c, &request, requests.VerifyCodePhone); !ok {
 		return
 	}
-
 	// 2. 发送 SMS
 	if ok := verifycode.NewVerifyCode().SendSMS(request.Phone); !ok {
 		response.Abort500(c, "发送短信失败~")
+	} else {
+		response.Success(c)
+	}
+}
+
+// SendUsingEmail 发送 Email 验证码
+func (vc *VerifyCodeController) SendUsingEmail(c *gin.Context) {
+
+	// 1. 验证表单
+	request := requests.VerifyCodeEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.VerifyCodeEmail); !ok {
+		return
+	}
+
+	// 2. 发送邮件
+	err := verifycode.NewVerifyCode().SendEmail(request.Email)
+	if err != nil {
+		response.Abort500(c, "发送 Email 验证码失败~")
 	} else {
 		response.Success(c)
 	}
